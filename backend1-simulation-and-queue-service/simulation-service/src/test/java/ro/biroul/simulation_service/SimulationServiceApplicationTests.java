6549c107817fa0970cf5bacd118d5d066273c069
IasminaPagu <paguiasmina0508@gmail.com>
2026-01-13 22:49:05 +0200

    Set-up SpringBoot backend services and configured PostgreSQL for backend2

---
+++ backend1-simulation-and-queue-service/simulation-service/src/test/java/ro/biroul/simulation_service/SimulationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.biroul.simulation_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimulationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
